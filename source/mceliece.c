6a213b740f80c11d4f40fc978bcbf4bd1a2c7eb0
filipondios <danialhamilla262@gmail.com>
2025-07-20 20:50:27 +0200

    build a valid shuffled P generation

--- source/mceliece.c
+++ source/mceliece.c
@@ -1,35 +1,59 @@
+#include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
 #include <sodium/randombytes.h>
 #include "mceliece.h"
 #include "matrix.h"
+#include "bits.h"
 
 
 void generate_s(uint8_t s[MATRIX_S_DIM]) {
     for (;;) {
+        // S = Random 4x4 invertible matrix
         randombytes_buf(s, MATRIX_S_DIM);
         uint8_t dummy[MATRIX_S_DIM];
 
         if (gauss(s, dummy))
             break;
     }
+
+    // Clean up unused bits in the S matrix
+    // Rows are stored at the 4 MSBs of s[i]
+    for (int i = 0; i < MATRIX_S_DIM; ++i) {
+        s[i] &= 0xf0;
+    }
 }
 
 void generate_p(uint8_t p[MATRIX_P_DIM]) {
-    uint8_t idxs[MATRIX_P_DIM] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
-    const uint8_t id [7] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40 };
-    memcpy(p, id, MATRIX_P_DIM);
+    uint8_t indices[MATRIX_P_DIM];
+    const uint8_t fixed = (randombytes_random() % MATRIX_P_DIM);
+    memset(p, 0x0, MATRIX_P_DIM);
 
-    // Fisher-Yates shuffle
-    for (uint8_t i = MATRIX_P_DIM - 1; i > 0; --i) {
-        const uint8_t j = randombytes_random() % (i + 1);
-        const uint8_t tmp = idxs[i];
-        idxs[i] = idxs[j];
-        idxs[j] = tmp;
+    for (uint8_t i = 0x0; i < MATRIX_P_DIM; ++i) {
+        // indices[row] = active column bit. Here indices
+        // is being initialized as the identity matrix.
+        indices[i] = i;
     }
 
-    for (uint8_t i = 0; i < MATRIX_P_DIM; ++i)
-        p[i] = id[idxs[i]];
+    // Fisher-Yates shuffle, except for one position
+    for (uint8_t i = MATRIX_P_DIM - 0x1; i > 0x0; --i) {
+        if (i == fixed) { continue; }
+        uint8_t j;
+
+        do {
+            randombytes_buf((void*) &j, sizeof(uint8_t));
+            j %= (i + 1);
+        } while (j == fixed);
+
+        const uint8_t tmp = indices[i];
+        indices[i] = indices[j];
+        indices[j] = tmp;
+    }
+
+    // Build the shuffled identity matrix
+    for (uint8_t i = 0; i < MATRIX_P_DIM; ++i) {
+        SET(p, i, indices[i]);
+    }
 }
 
 void keygen(PublicKey* publickey, PrivateKey* secretkey) {
