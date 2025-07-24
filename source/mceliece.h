42452cc9a34a2260d15287678623841c093e722a
filipondios <danialhamilla262@gmail.com>
2025-07-24 22:00:08 +0200

    add encode, decode, encrypt and decrypt funcion definitions

--- source/mceliece.h
+++ source/mceliece.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stddef.h>
 #include <stdint.h>
 #include "matrix.h"
 
@@ -16,4 +17,16 @@ typedef struct {
 
 void generate_s(uint8_t s[MATRIX_S_DIM]);
 void generate_p(uint8_t p[MATRIX_P_DIM]);
-void keygen(PublicKey* publickey, PrivateKey* secretkey);
+void keygen(PublicKey* public_key, PrivateKey* secret_key);
+
+uint8_t encode(const uint8_t block, const uint8_t sgp[MATRIX_SGP_ROWS]);
+
+int encrypt(const PublicKey* public_key, const uint8_t* in,
+            const size_t in_len, uint8_t** out, size_t* out_len);
+
+uint8_t decode(const uint8_t block, const uint8_t s_inv[MATRIX_S_DIM],
+               const uint8_t p[MATRIX_P_DIM], const uint8_t h[MATRIX_H_ROWS],
+               const uint8_t g[MATRIX_G_ROWS]);
+
+int decrypt(const PrivateKey* private_key, const uint8_t* cipher,
+            const size_t cipher_len, uint8_t** out, size_t* out_len);
